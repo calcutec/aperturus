49038294ce0d2d8329a9919c15391ec0fd0ee8fd
William Burton <burton.wj@gmail.com>
2016-01-25 14:35:36 -0500

    updated env config

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 CSRF_ENABLED = True
-SECRET_KEY = "Formetoknow"
+SECRET_KEY = os.environ['FLASK_SECRET_KEY']
 
 
 SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
